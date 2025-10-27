e9e6e514f31f5961e12b6c882b16eb6e1bd49195
Neel-R-Patel <neelrpatel0002@gmail.com>
2025-10-27 12:59:12 +0530

    Initial commit of static website files

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline {
+  agent any
+
+  environment {
+    S3_BUCKET = 'jnkin'          // your S3 bucket name
+    CLOUDFRONT_ID = 'E3H5J8BMQF0AYA'          // your CloudFront Distribution ID
+    AWS_REGION = 'us-east-1'                // your AWS region
+  }
+
+  stages {
+    stage('Deploy to S3') {
+      steps {
+        withAWS(credentials: 'aws-cred', region: "${AWS_REGION}") {
+          // Upload all files to S3, remove old ones
+          sh "aws s3 sync ./ s3://${S3_BUCKET} --exclude '.git/*' --delete"
+        }
+      }
+    }
+
+    stage('Invalidate CloudFront') {
+      steps {
+        withAWS(credentials: 'aws-cred', region: "${AWS_REGION}") {
+          // Clear CloudFront cache
+          sh "aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths '/*'"
+        }
+      }
+    }
+  }
+}
